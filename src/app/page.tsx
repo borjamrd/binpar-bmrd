d902ced7cda5b9dee55f8104811e750d9028a667
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-03-11 21:26:59 +0100

    chore: loading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,95 @@
+"use client"
+
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import Loading from "@/components/Loading";
+import { PageTransitionLayout } from "@/components/PageTransitionsLayout";
+import { useGlobalContext } from "@/context/Global";
+import { Pokemon, TYPE } from "@/models/models";
 import Link from "next/link";
+import { useState, ReactNode } from "react";
 
 export default function HomePage() {
+
+  const { allPokemonData, realTimeSearch, searchResults, loading, next } =
+    useGlobalContext();
+
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e: TYPE) => {
+    setSearch(e.target.value);
+    realTimeSearch(search);
+  };
+
+  const handleSearch = (e: TYPE) => {
+    e.preventDefault();
+    realTimeSearch(search);
+  };
+
+  const displaySearchedPokemon = (): ReactNode => {
+    return searchResults.map((pokemon: Pokemon) => {
+      return (
+        <Link
+          href={pokemon.name}
+          className="px-2 py-0 text-lg"
+          key={pokemon.name}
+        >
+          {pokemon.name?.slice(0, 1)?.toUpperCase() + pokemon.name?.slice(1)}
+        </Link>
+      );
+    });
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
+    <PageTransitionLayout>
+      <main className="flex flex-col justify-between items-center p-10 min-h-screen">
+        <>
+          <form
+
+            className="flex justify-center items-center mb-4 relative"
+            onSubmit={handleSearch}
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+            <div>
+              <input
+                className="px-4 py-5 bg-white border-slate-600 rounded shadow "
+                type="text"
+                value={search}
+                onChange={handleChange}
+                placeholder="Filtra pokemons por nombre"
+              />
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
+          </form>
+          {loading || (allPokemonData.length === 0 && <Loading />)}
+          {search && searchResults?.length > 0 && (
+            <div className="absolute h-56 w-56 top-1/4 left-1/2 overflow-auto transform translate-x-1/2 z-10 px-4 py-5 rounded bg-white shadow flex flex-col">
+              {displaySearchedPokemon()}
             </div>
-          </Link>
-        </div>
-      </div>
-    </main>
+          )}
+          <div className="grid grid-cols-1 max-w-[320px]">
+            {allPokemonData.map((pokemon: Pokemon) => {
+              return <div key={pokemon.id}>Pokemon card</div>;
+            })}
+          </div>
+          <div className="next">
+            {!loading && allPokemonData.length > 0 ? (
+              <div>
+                <button className="flex justify-center items-center mt-4 p-4 bg-white border border-opacity-50 border-gray-200 rounded-lg shadow-md text-gray-700 cursor-pointer" onClick={next}>
+                  Cargar más &darr;
+                </button>
+              </div>
+            ) : (
+              <div>
+                <button className="flex justify-center items-center mt-4 p-4 bg-white border border-opacity-50 border-gray-200 rounded-lg shadow-md text-gray-700 cursor-pointer" disabled>
+                  Cargando...
+                </button>
+              </div>
+            )}
+          </div>
+        </>
+      </main>
+    </PageTransitionLayout>
   );
 }
